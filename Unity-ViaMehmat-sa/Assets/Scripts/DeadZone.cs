9f3f300b2fcc7ea7aa75c4def6b8af475991a291
maarr1 <mafrolo@sfedu.ru>
2024-11-17 00:24:41 +0300

    изменение иерархии файлов

--- Unity-ViaMehmat-sa/Assets/Scripts/DeadZone.cs
+++ Unity-ViaMehmat-sa/Assets/Scripts/DeadZone.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DeadZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "crow")
+        if (collision.gameObject.tag == "Crow")
         {
             Destroy(collision.gameObject);
         }
