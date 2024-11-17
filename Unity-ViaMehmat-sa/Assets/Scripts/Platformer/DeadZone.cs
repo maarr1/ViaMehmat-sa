d7ed23db809bfa9a6d8d5e0c516dea7008aa45c7
maarr1 <mafrolo@sfedu.ru>
2024-11-17 11:41:14 +0300

    основные аспекты кода для платформера

--- Unity-ViaMehmat-sa/Assets/Scripts/Platformer/DeadZone.cs
+++ Unity-ViaMehmat-sa/Assets/Scripts/Platformer/DeadZone.cs
@@ -1,10 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DeadZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Crow")
+        if (collision.gameObject.tag == "crow")
         {
             Destroy(collision.gameObject);
         }
